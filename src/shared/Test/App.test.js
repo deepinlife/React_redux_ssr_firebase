9708a7e8dfe28ba4bec216f56b1b4516b8f41ec2
Praveen Kumar <40301900+deepinlife@users.noreply.github.com>
2018-08-08 14:12:57 +0530

    Update App.test.js

--- src/shared/Test/App.test.js
+++ src/shared/Test/App.test.js
@@ -5,7 +5,6 @@ import Login from '../Login.js';
 import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Redirect } from 'react-router-dom';
-import { wrap } from 'module';
 
 configure({ adapter: new Adapter() });
 describe("<App />", () => {
@@ -24,16 +23,7 @@ describe("<App />", () => {
         wrapper.setState({ signup: false, login: false });
         expect(wrapper.find(Redirect)).toHaveLength(0);
     });
-    it('passes login information', () => {
-        const email = 'pk@gmail.com';
-        const password = 'password';
-        const wrapper = mount(<Login />);
-        const button = wrapper.find('button');
-        button.simulate('click');
-        wrapper.update();
-        expect(wrapper.state().send).toEqual(false);
-        console.log(wrapper.state());
-    });
+   
 
 
 
